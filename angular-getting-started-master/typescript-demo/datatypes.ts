7651f4b95e1306842ae37360770fe1ec7d5112e6
ampinkus <ampinkus@gmail.com>
2022-12-25 16:06:12 -0300

    https://youtu.be/3qBXWUpoPHo?t=5414

--- angular-getting-started-master/typescript-demo/datatypes.ts
+++ angular-getting-started-master/typescript-demo/datatypes.ts
@@ -1,4 +1,4 @@
-let lname: string = "Pinkus";
+/* let lname: string = "Pinkus";
 console.log(lname);
 lname = "Alfredo";
 let newName = lname.toUpperCase();
@@ -23,3 +23,60 @@ console.log(employees);
 let names: Array<string>; // another way to define an Array
 names = ["Alfredo", "Tomas", "Juan"];
 console.log(names);
+
+let numList: Array<number>; // another way to define an Array
+numList = [1, 2, 3, 4, 5];
+console.log(numList);
+
+let filtered = numList.filter((num) => num > 2); // filter the numbers that are > 2
+console.log(filtered);
+
+let finded = numList.find((num) => num > 2); // return the first number that comply with the condition
+console.log(finded);
+
+let other = numList.find((num) => num == 5); // return the number if it is there
+console.log(other);
+
+let findName = employees.find((findName) => (findName = "Alfredo")); // we can use findname in all the sentence
+console.log(findName);
+
+let numList2: Array<number>; // another way to define an Array
+numList2 = [1, 2, 3, 4, 5];
+
+let suma = numList2.reduce((acc, num) => acc + num);
+console.log(suma);
+
+//enum
+enum Color {
+  red,
+  green,
+  blue,
+}
+let color = Color.blue;
+console.log(Color);
+console.log(color);
+
+const enum Color2 {
+  red,
+  green,
+  blue,
+}
+let color2 = Color2.blue; // when it compile to JS it assign to color the index value of blue
+console.log(color2);
+*/
+
+// tuples
+let swapNumbs: [firstNumber: number, secondNumber: number];
+function swapNumbers(num1: number, num2: number): [number, number] {
+  return [num2, num1];
+}
+
+swapNumbs = [10, 20];
+console.log(swapNumbs);
+console.log(swapNumbs[0]);
+swapNumbs = swapNumbers(10, 20);
+console.log(swapNumbs);
+
+let code: any;
+code = 10;
+code = "asd";
---
+++ angular-getting-started-master/typescript-demo/functions.ts
@@ -0,0 +1,42 @@
+function add(a: number, b: number): number {
+  // need to return number
+  return a + b;
+}
+
+const addArrow = (a: number, b: number): number => a + b;
+
+const mult = function (a: number, b: number): number {
+  // need to return number
+  return a * b;
+};
+
+// optional parameters. if c exists return a + b + c, else a + b
+const addThree = (a: number, b: number, c?: number): number =>
+  c ? a + b + c : a + b;
+
+//Required parameter, if c not specified we use 10
+const addRequired = (a: number, b: number, c: number = 10): number => a + b + c;
+
+console.log(add(1, 2));
+console.log(addArrow(1, 2));
+console.log(mult(3, 2));
+console.log(addThree(1, 2, 3));
+console.log(addThree(1, 2));
+console.log(addRequired(1, 2));
+
+// Rest operator, we can use a undefined number of arguments
+const addReduce = (a: number, b: number, ...c: number[]): number =>
+  a + b + c.reduce((a, b) => a + b, 0);
+
+let numbers: number[] = [3, 4, 5];
+console.log(addReduce(1, 2, ...numbers));
+
+//generic function
+function getItems<Type>(items: Type[]): Type[] {
+  return new Array<Type>().concat(items);
+}
+
+let concatNumbers = getItems([1, 2, 3, 4, 5]);
+let concatString = getItems(["a", "b", "c", "d", "e"]);
+console.log(concatNumbers);
+console.log(concatString);
