19b5e410fe8ac6fc1f517dc5969f47d731a17a80
ampinkus <ampinkus@gmail.com>
2022-12-29 15:22:25 -0300

    hotelinventoryapp added first binding

--- HotelInventoryApp/src/app/rooms/rooms.component.ts
+++ HotelInventoryApp/src/app/rooms/rooms.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.scss']
+  styleUrls: ['./rooms.component.scss'],
 })
 export class RoomsComponent implements OnInit {
+  hotelName: string = 'Hilton Hotel';
+  numberOfRooms: any = 10;
+  hideRooms = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+    console.log(this.hideRooms);
   }
-
 }
