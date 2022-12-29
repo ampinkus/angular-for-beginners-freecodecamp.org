f51d36d0ccd34721e9cbbbfcfd5c0dc943312bce
ampinkus <ampinkus@gmail.com>
2022-12-29 14:45:39 -0300

    first commit hotelinventoryapp

--- HotelInventoryApp/src/app/app.component.ts
+++ HotelInventoryApp/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'hinv-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'HotelInventoryApp';
+  title = 'hotelinventoryapp';
 }
