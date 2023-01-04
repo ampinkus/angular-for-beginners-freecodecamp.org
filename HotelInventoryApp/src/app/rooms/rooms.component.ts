2fc192b5aabb6efcdef7aba1b864cfa6a7b36435
ampinkus <ampinkus@gmail.com>
2023-01-04 14:45:42 -0300

    Added list of rooms

--- HotelInventoryApp/src/app/rooms/rooms.component.ts
+++ HotelInventoryApp/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, RoomList } from '../room';
 
 @Component({
   selector: 'hinv-rooms',
@@ -9,7 +10,50 @@ export class RoomsComponent implements OnInit {
   hotelName: string = 'Hilton Hotel';
   addressNumber: number = 867;
   numberOfRooms: any = '10 single + 20 suites';
-  hideRooms = true;
+  hideRooms = false;
+  hotelObject = {
+    Category: 'Premium',
+    Age: 10,
+    Inaugurated: 1976,
+  };
+
+  // we use the object of the interface
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  //create a list of different rooms
+  roomList: RoomList[] = [
+    {
+      roomType: 'Deluxe room',
+      amenities: 'Air Conditioner, WiFi, Tv, bathroom, kitchen',
+      price: 500,
+      photos:
+        'https://images.unsplash.com/photo-1631049307264-da0ec9d70304?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+      checkinTime: new Date('11-jan-2023'),
+      checkoutTime: new Date('12-jan-2023'),
+    },
+    {
+      roomType: 'Standard room',
+      amenities: 'Air Conditioner, Tv, bathroom',
+      price: 300,
+      photos:
+        'https://images.unsplash.com/flagged/photo-1556438758-8d49568ce18e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80',
+      checkinTime: new Date('21-jan-2023'),
+      checkoutTime: new Date('23-jan-2023'),
+    },
+    {
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, WiFi, Tv, bathroom, kitchen, living room',
+      price: 800,
+      photos:
+        'https://images.unsplash.com/photo-1566195992011-5f6b21e539aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+      checkinTime: new Date('17-jan-2023'),
+      checkoutTime: new Date('20-jan-2023'),
+    },
+  ];
 
   constructor() {}
 
